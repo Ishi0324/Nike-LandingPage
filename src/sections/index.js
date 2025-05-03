4d29f12d347247f401d62e793387e04edfc321dd
Ishi0324 <98327719+Ishi0324@users.noreply.github.com>
2025-05-03 18:37:55 +0530

    Add files via upload

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProduct from "./PopularProduct";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import CustomerReview from "./CustomerReview";
+
+export {
+    Hero,
+    PopularProduct,
+    Services,
+    SpecialOffers,
+    SuperQuality,
+    Subscribe,
+    Footer,
+    CustomerReview
+}
